c439e942c8d3d083a8dbcc470b6ce54a6007511e
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-10-02 14:11:25 -0700

    reformatted for mobile-first design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,37 @@
 export default function Sidebar() {
     return (
-    <div id="sidebar">
-        <img id="logo" src="./assets/icons/logo-dark.svg"></img>
-        <h3 id="board-header">ALL BOARDS</h3>
-        <div className="board-selector selected-board">
+        <div id="sidebar">
+            <img id="logo" src="./assets/icons/logo-mobile.svg" />
+            <h2 id="board-header">Platform Launch</h2>
             <img
-                style="height: 2ch;"
-                src="./assets/icons/icon-board-selected.svg"
-            ></img>
-            <p>Platform Launch</p>
+                id="board-chevron"
+                src="./assets/icons/icon-chevron-down.svg"
+            />
+            <img
+                id="add-task-btn"
+                src="./assets/icons/icon-add-task-mobile.svg"
+            />
+            <img
+                id="board-settings"
+                src="./assets/icons/icon-vertical-ellipsis.svg"
+            />
+            <div id="board-header-container">
+                <div id="board-header-list">
+                    <h3 id="board-list-title">All Boards</h3>
+                    <div className="board-name board-name-selected">
+                        <img src="./assets/icons/icon-board-selected.svg" />
+                        <h3>Platform Launch</h3>
+                    </div>
+                    <div className="board-name">
+                        <img src="./assets/icons/icon-board.svg" />
+                        <h3>Marketing Plan</h3>
+                    </div>
+                    <div className="board-name">
+                        <img src="./assets/icons/icon-board.svg" />
+                        <h3>Roadmap</h3>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="board-selector">
-            <img style="height: 2ch;" src="./assets/icons/icon-board.svg"></img>
-            <p>Marketing Plan</p>
-        </div>
-        <div className="board-selector">
-            <img style="height: 2ch;" src="./assets/icons/icon-board.svg"></img>
-            <p>Roadmap</p>
-        </div>
-    </div>
     );
 }
