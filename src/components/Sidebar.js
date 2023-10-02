67152515e071556958fda096ce119b9c96d6e1d1
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-10-02 13:15:41 -0700

    updated styles, component directory

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+export default function Sidebar() {
+    return (
+    <div id="sidebar">
+        <img id="logo" src="./assets/icons/logo-dark.svg"></img>
+        <h3 id="board-header">ALL BOARDS</h3>
+        <div className="board-selector selected-board">
+            <img
+                style="height: 2ch;"
+                src="./assets/icons/icon-board-selected.svg"
+            ></img>
+            <p>Platform Launch</p>
+        </div>
+        <div className="board-selector">
+            <img style="height: 2ch;" src="./assets/icons/icon-board.svg"></img>
+            <p>Marketing Plan</p>
+        </div>
+        <div className="board-selector">
+            <img style="height: 2ch;" src="./assets/icons/icon-board.svg"></img>
+            <p>Roadmap</p>
+        </div>
+    </div>
+    );
+}
